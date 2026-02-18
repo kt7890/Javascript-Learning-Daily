49fe24ad84f95f91a5aaaaf53034cc25fd798413
Kamlakar Tiwari <mejk768@gmail.com>
2026-02-18 12:55:41 +0530

    Day 37: Custom implementation of Array.reduce

---
+++ Day37-Custom-Reduce/index.js
@@ -0,0 +1,23 @@
+Array.prototype.myReduce = function (callback, initialValue) {
+  let accumulator = initialValue;
+  let startIndex = 0;
+
+  if (accumulator === undefined) {
+    accumulator = this[0];
+    startIndex = 1;
+  }
+
+  for (let i = startIndex; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
+
+  return accumulator;
+};
+
+const numbers = [1, 2, 3, 4];
+
+const sum = numbers.myReduce((acc, curr) => acc + curr, 0);
+console.log(sum);
+
+const product = numbers.myReduce((acc, curr) => acc * curr);
+console.log(product);
