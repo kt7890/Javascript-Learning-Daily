1b8e3745e0f9b3a1a48c923d574a0c976f084823
Kamlakar Tiwari <mejk768@gmail.com>
2026-02-18 12:54:52 +0530

    Day 36: Custom implementation of Array.filter

---
+++ Day36-Custom-Filter/index.js
@@ -0,0 +1,17 @@
+Array.prototype.myFilter = function (callback) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+
+  return result;
+};
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evenNumbers = numbers.myFilter((num) => num % 2 === 0);
+
+console.log(evenNumbers);
